c808e1480df03eebc8518daec534064f1a5e6ae9
ibnul42 <ibnulashir42@gmail.com>
2021-04-26 12:04:51 +0600

    Blog page completed

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import allBlogs from '../Components/AllBlogs';
+import Title from '../Components/Title';
 
 const BlogsPage = () => {
     return (
         <div>
-            <h1>Blog Page</h1>
+            <div className="b-title">
+                 <Title title={'My Blogs'} span={'Recent blogs'}></Title>
+            </div>
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog) => {
+                        return <div className="blog" key={blog.id}>
+                                    <div className="blog-content">
+                                        <img src={blog.image} alt=""/>
+                                        <a href={blog.link} className="blog-link">
+                                            {blog.title}
+                                        </a>
+                                    </div>
+                                </div>
+                    })
+                }
+            </div>
         </div>
     );
 };
