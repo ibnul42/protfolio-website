e040490fabe5b1e2cf1124cfe03fe17e39241e93
ibnul42 <ibnulashir42@gmail.com>
2021-05-05 13:07:22 +0600

    protfolio page completed

--- src/Pages/ProtfolioPage.js
+++ src/Pages/ProtfolioPage.js
@@ -5,16 +5,30 @@ import MenuItems from '../Components/MenuItems';
 import protfolios from '../Components/allProtfolios';
 import { useState } from 'react';
 
+const allCategories =['All', ...new Set(protfolios.map(item => item.category))];
+console.log(allCategories)
+
 const ProtfolioPage = () => {
-    const [categories, setCategories] = useState(null);
+    const [categories, setCategories] = useState(allCategories);
     const [menuItems, setMenuItems] = useState(protfolios);
+
+    const filter = (category) => {
+        if(category === 'All'){
+            setMenuItems(protfolios);
+            return;
+        }
+        const filterData = protfolios.filter((item) =>{
+            return item.category === category;
+        })
+        setMenuItems(filterData);
+    }
     return (
         <div className="ProtfolioPage">
             <div className="title">
                 <Title title={'Protfolio'} span={'Protfolio'} />
             </div>
             <div className="protfolios-data">
-                {/* <Categories /> */}
+                <Categories filter={filter} categories={categories}/>
                 <MenuItems menuItem={menuItems} />
             </div>
         </div>
